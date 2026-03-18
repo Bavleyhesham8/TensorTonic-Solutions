cfc2ae32d2427c70704cafc0357f3d1282362269
Bavley Hesham <125792020+Bavleyhesham8@users.noreply.github.com>
2026-03-18 10:28:01 +0200

    Add solution for binomial-pmf-cdf

---
+++ binomial-pmf-cdf/binomial-pmf-cdf.py
@@ -0,0 +1,19 @@
+import numpy as np
+from scipy.special import comb
+
+def binomial_pmf_cdf(n, p, k):
+    """
+    Compute Binomial PMF and CDF.
+    """
+    # Write code here
+    if not (isinstance(n, int) and n >= 0):
+        raise ValueError("n must be a non-negative integer.")
+    if not (0 <= p <= 1):
+        raise ValueError("p must be between 0 and 1.")
+    if not (isinstance(k, int) and 0 <= k <= n):
+        raise ValueError("k must be an integer between 0 and n.")
+    pmf = comb(n, k, exact=False) * (p ** k) * ((1 - p) ** (n - k))
+    y = np.arange(0, k + 1)
+    cdf = np.sum(comb(n, y, exact=False) * (p ** y) * ((1 - p) ** (n - y)))
+    return float(pmf), float(cdf)
+    pass
